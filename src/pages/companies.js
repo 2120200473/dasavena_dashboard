bef82450a7acab277fa740f649cbe29e3bfb0ca5
2120200473 <116007485+2120200473@users.noreply.github.com>
2023-10-11 11:46:30 -0600

    Eliminar codigo de customers
    Se elimina codigo sin funcionamiento en el dashboard

--- src/pages/companies.js
+++ src/pages/companies.js
@@ -1,22 +1,3 @@
-import Head from 'next/head';
-import ArrowUpOnSquareIcon from '@heroicons/react/24/solid/ArrowUpOnSquareIcon';
-import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
-import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
-import {
-  Box,
-  Button,
-  Container,
-  Pagination,
-  Stack,
-  SvgIcon,
-  Typography,
-  Unstable_Grid2 as Grid
-} from '@mui/material';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { CompanyCard } from 'src/sections/companies/company-card';
-import { CompaniesSearch } from 'src/sections/companies/companies-search';
-
-
 import React from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
@@ -24,59 +5,6 @@ import "survey-core/defaultV2.min.css";
 import { json } from "./json";
 import { themeJson } from "./theme";
 
-const companies = [
-
-];
-
-const Page = () => (
-  <>
-    <Head>
-      <title>
-        Expediente Digital | Dasavena
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="xl">
-        <Stack spacing={3}>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            spacing={4}
-          >
-            <Stack spacing={1}>
-              <Typography variant="h4">
-                Expediente Digital
-              </Typography>
-              <Stack
-                alignItems="center"
-                direction="row"
-                spacing={1}
-              >
-                
-              </Stack>
-            </Stack>
-            
-          </Stack>
-
-          
-        </Stack>
-      </Container>
-      
-    </Box>
-  </>
-);
-
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
 
 function SurveyComponent() {
   const survey = new Model(json);
